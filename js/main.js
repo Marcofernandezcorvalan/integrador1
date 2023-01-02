022a26c9335429bf83a7bba9207128a63efd4f6c
Marcofernandezcorvalan <marcofernandezc@outlook.com>
2023-01-02 16:53:44 -0300

    Cambio de html, agr buttons, array products

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// contenedor de productos
+const products = document.querySelector(".products__container");
+// contenedor de las categorias
+const categories = document.querySelector(".products__categories");
+// html collection de las categorias
+const categoriesData = document.querySelector(".products__categories__category");
+// modal
+const modal = document.querySelector(".modal");
