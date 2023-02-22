7320d9ec6496f8791dd766db61d4b3ffdfc547af
Marcofernandezcorvalan <marcofernandezc@outlook.com>
2023-02-22 20:18:22 -0300

    logica del carrito. Y me trabé porque me da undefined el precio.

--- js/main.js
+++ js/main.js
@@ -20,11 +20,127 @@ const logReg = document.querySelector(".header__cont");
 const cart = document.querySelector(".cartLabel");
 // cart Container
 const cartCont = document.querySelector(".cart");
+// contenedor de las cards del carrito
+const cardsCont = document.querySelector(".cardsContainer");
+// total del carrito
+const total = document.querySelector(".cartTotal");
+// botón comprar en el carrito
+const BuyBtn = document.querySelector(".botonCartBuy");
+// botón limpiar en el carrito
+const CleanBtn = document.querySelector(".botonCartClean");
+
+// Carrito
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const saveLocalStorage = (carritoLista) => {
   localStorage.setItem("carrito", JSON.stringify(carritoLista));
+};
+
+const mostrarProductoCart = (cartProduct) => {
+  const { id, name, img, precio, quantity } = cartProduct;
+  return `
+  <div class="cartItem">
+          <img src="${img}" />
+          <div class="itemInfo">
+            <h3 class="itemTitle">${name}</h3>
+            <span class="itemPrecio">U$D ${precio} </span>
+          </div>
+          <div class="itemCantidad">
+            <span class="cantidadNum rest" data-id="${id}">-</span>
+            <span class="cantidadNum">${quantity}</span>
+            <span class="cantidadNum sum" data-id="${id}">+</span>
+          </div>
+        </div>
+  `;
+};
+
+const renderCart = () => {
+  if (!carrito.length) {
+    cardsCont.innerHTML = ` <p class="emptyMsg">No hay Productos en el Carrito</p>`;
+    return;
+  }
+  cardsCont.innerHTML = carrito.map(mostrarProductoCart).join("");
+};
+
+const totalCarrito = () => {
+  return carrito.reduce((acc, cur) => {
+    return acc + Number(cur.precio) * cur.quantity;
+  }, 0);
+};
+
+const mostrarTotal = () => {
+  total.innerHTML = `<div class="totalCont">U$D ${totalCarrito()}</div>`;
+};
+
+const desabilitarBtn = (btn) => {
+  if (!carrito.length) {
+    btn.classList.add("disabled");
+  } else {
+    btn.classList.remove("disabled");
+  }
+};
+
+const checkCart = () => {
+  saveLocalStorage(carrito);
+  renderCart();
+  mostrarTotal();
+  desabilitarBtn(BuyBtn);
+  desabilitarBtn(CleanBtn);
+};
+
+const agregarProducto = (e) => {
+  if (!e.target.classList.contains("products__container__card__scont--btn")) {
+    return;
+  }
+  const { id, name, img, precio } = e.target.dataset;
+  const product = productsData(id, name, img, precio);
+
+  if (existeEnCarrito(product)) {
+    agregarUnidadACard(product);
+    showSuccessModal("Se añadió una unidad");
+  } else {
+    crearCardCarrito(product);
+    showSuccessModal("Producto añadido con Éxito");
+  }
+
+  checkCart();
+};
+
+const productsData = (id, name, img, precio) => {
+  return { id, name, img, precio };
+};
+
+const existeEnCarrito = (product) => {
+  return carrito.find((item) => {
+    return item.id === product.id;
+  });
+};
+
+const agregarUnidadACard = (product) => {
+  carrito = carrito.map((cartProduct) => {
+    return cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct;
+  });
+};
+
+const showSuccessModal = (msg) => {
+  modal.classList.add("modalActivo");
+  modal.textContent = msg;
+  setTimeout(() => {
+    modal.classList.remove("modalActivo");
+  }, 1000);
+};
+
+const crearCardCarrito = (product) => {
+  carrito = [
+    ...carrito,
+    {
+      ...product,
+      quantity: 1,
+    },
+  ];
 };
 
 // Renderizar los Productos
@@ -179,6 +295,11 @@ const init = () => {
   cart.addEventListener("click", toggleCart);
   barsMenu.addEventListener("click", cerrar);
   window.addEventListener("scroll", cerrarEnScrolleo);
+  document.addEventListener("DOMContentLoaded", renderCart);
+  document.addEventListener("DOMContentLoaded", mostrarTotal);
+  document.addEventListener("DOMContentLoaded", desabilitarBtn(BuyBtn));
+  document.addEventListener("DOMContentLoaded", desabilitarBtn(CleanBtn));
+  products.addEventListener("click", agregarProducto);
 };
 
 init();
